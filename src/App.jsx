85048ecddc8423b5f848d44ba48d4e87ccb6b1ed
birhane <birhane457@gmail.com>
2026-07-02 04:34:11 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -109,7 +109,7 @@ const AuthenticatedApp = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
